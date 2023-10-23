194c4f4f1b6a518d27266d438f443ecfff993529
alecanonm <al3jandrocan0n@gmail.com>
2023-10-23 17:39:19 -0500

    helping to edgar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 import TodoList from "./components/TodoList/TodoList";
 import TodoForm from "./components/TodoForm/TodoForm";
 import CategoryFilter from "./components/CategoryFilter/CategoryFilter";
 
 function App() {
   return (
-    <div className="bg-red-500">
+    <div className="">
       <TodoForm />
       <CategoryFilter />
       <TodoList />
