d256273544c0a4cee3c75135dea21a6dc12463f3
edgartellom <edgar.tello@tecsup.edu.pe>
2023-10-24 23:54:01 -0300

    fix deploy

--- client/src/components/TodoList/TodoList.jsx
+++ client/src/components/TodoList/TodoList.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import TodoItem from "../TodoItem/TodoItem";
-import axios from "axios";
-import CategoryFilter from "../CategoryFilter/CategoryFilter";
 
 const TodoList = ({ todos }) => {
   return (
